9c0c518953744234f2e01ece5622be1b66f7fc76
Tom2bay <zytom@pobox.com>
2026-04-24 22:35:38 -0700

    Add Vite for frontend dev

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: process.env.VITE_BASE || "/",
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: true,
+    port: 8283,
+    // strictPort: true,
+    // open: true // Open browser on server start
+    // cors: true,
+    proxy: {
+      // Proxy requests starting with '/api' backend URL
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
